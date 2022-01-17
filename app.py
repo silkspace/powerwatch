0a70cd33fbeb43e7c46473e9f3480af76a9ce645
Alex <alex@stayopen.com>
2022-01-16 16:34:26 -0800

    power and influence app for search and research

---
+++ app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import graphistry
+
+from src.utils import search_to_df, get_graphistry_from_search, \
+    get_graphistry_from_milieu_search, setup_logger
+
+
+logger = setup_logger(__name__)
+
+src, dst, node_col = 'to_node', 'from_node', 'Node'
+
+@st.cache
+def get_data(dummy_variable):
+    logger.info('Loading Data')
+    edges = pd.read_csv('data/edges.csv', index_col=0)
+    edges = edges.fillna('')
+    nodes = pd.read_csv('data/nodes.csv', index_col=0)
+    nodes = nodes.fillna('')
+    return edges, nodes
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+def display_graph(g):
+    url = g.plot(render=False)
+    st.markdown(f'<iframe width=1000 height=800 src="{url}"></iframe>', unsafe_allow_html=True)
+
+@st.cache
+def simple_search(search_term):
+    g = get_graphistry_from_search(search_term, src, dst, node_col, edf, ndf)
+    return g
+@st.cache
+def milieu_search(search_term, both=False):
+    g = get_graphistry_from_milieu_search(search_term, src, dst, node_col, edf, ndf, both=both)
+    return g
+
+@st.cache
+def text_search(search_term):
+    res = pd.concat([search_to_df(search_term, 'Summary', ndf), search_to_df(search_term, 'Blurb', ndf)], axis=0)
+    tdf = edf[edf.to_node.isin(res.Node)]
+    g = graphistry.edges(tdf, src, dst).nodes(res, node_col)
+    return g
+
+edf, ndf = get_data(True)
+
+# search bar
+st.sidebar.header('Influence & Power Networks')
+st.sidebar.subheader('Search Network')
+
+rand = st.sidebar.checkbox('Get Random')
+if rand:
+    entity_options = ndf.Node
+    entity = entity_options.sample(1).values[0]
+else:
+    entity = 'BlackRock'
+    
+local_or_milieu = st.sidebar.checkbox('Local or Milieu Search')
+search_summary_and_blurb = st.sidebar.checkbox('Search Text', value=~local_or_milieu)
+
+search_term = st.sidebar.text_input('', entity, key='search')
+
+#get the graph
+if search_summary_and_blurb:
+    g = text_search(search_term)
+    st.write(g._nodes)
+elif local_or_milieu:
+    g = simple_search(search_term)
+else:
+    g = milieu_search(search_term)
+    
+display_graph(g)
+
+# logo
+st.sidebar.write("-" * 40)
+image = Image.open("./tensorml.png")
+st.sidebar.image(image, use_column_width=True)
---
+++ src/utils.py
@@ -0,0 +1,138 @@
+import graphistry
+import pandas as pd
+import logging
+
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+    logging.basicConfig(format=FORMAT)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+logger = setup_logger(__name__)
+
+graphistry.register(api=3, protocol="https", server="hub.graphistry.com", username="silkspace", password="yqQg02&N")
+
+# #################################################################################################
+#
+#   Finding subgraphs within a large graph.
+#
+# #################################################################################################
+
+def search_to_df(word, col, df):
+    """
+    A simple way to retrieve entries from a given col in a dataframe
+    :eg
+        search_to_df('BlackRock', 'to_node', edf)
+
+    :param word: str search term
+    :param col: given column of dataframe
+    :param df: pandas dataframe
+    :returns
+        DataFrame of results
+    """
+    try:
+        res = df[df[col].str.contains(word, case=False)]
+    except TypeError as e:
+        logger.error(e)
+        return df
+    return res
+
+# def df_to_graph(src, dst, node_col, edf, df):
+#     g = graphistry.edges(edf, src, dst).nodes(df, node_col)
+#     return g
+#
+# def search_to_graphistry(search_term, search_col, src, dst, node_col, edf, ndf):
+#     df = search_to_df(search_term, search_col, ndf)
+#     return df_to_graph(src, dst, node_col, edf, df)
+
+def get_nearest(search_term, src, dst, edf):
+    """
+    :param search_term:
+    :param src:
+    :param dst:
+    :param edf:
+    :return: pandas.DataFrame
+    """
+    logger.info(f"Finding {search_term} in both {src} and {dst} columns")
+    tdf = pd.concat(
+        [search_to_df(search_term, src, edf), search_to_df(search_term, dst, edf)],
+        axis=0,
+    )
+    return tdf
+
+
+def get_graphistry_from_search(search_term, src, dst, node_col, edf, ndf):
+    """
+        Useful helper function to get subgraph from a search term
+    :param search_term: Note this retrieves all nodes that have `search_term` in it -- ie, not strict matches
+    :param src:
+    :param dst:
+    :param node_col:
+    :param edf:
+    :param ndf:
+    :return:
+    """
+    tdf = get_nearest(search_term, src, dst, edf)
+    gcols = pd.concat(
+        [tdf[dst], tdf[src]], axis=0
+    )  # get all node entries that show up in edge graph
+    ntdf = ndf[ndf[node_col].isin(gcols)]
+    g = graphistry.edges(tdf, src, dst).nodes(ntdf, node_col)
+    return g
+
+
+# lets make simple functions to find subgraphs by search_term = a given node
+def get_milieu_graph_from_search(search_term, src, dst, edf, both=False):
+    """
+    Can think of this as finding all 2-hop connections from a given `search_term`. It will find all direct edges to
+    `search_term` as well as the edges of all those entities. It shows the `milieu graph` of the `search_term`
+    :param search_term:
+    :param src:
+    :param dst:
+    :param edf:
+    :param both: to retrieve edges from both src and dst columns of dataframe -- if true, returns a larger edgeDataFrame
+    :return:
+    """
+    # get all the entities in either column
+    # tdf = pd.concat([search_to_df(search_term, src, edf), search_to_df(search_term, dst, edf)], axis=0)
+    tdf = get_nearest(search_term, src, dst, edf)
+    # now find all their nearest connections.
+    if both:
+        gcols = pd.concat([tdf[dst], tdf[src]], axis=0)
+        logger.info(
+            f"Then finding all edges from {search_term} in {src} and {dst} columns of full edgeDataFrame"
+        )
+        df = edf[(edf[src].isin(gcols) | edf[dst].isin(gcols))]
+    else:
+        # logger.info(f'Finding {search_term} in {src} columns')
+        # tdf = search_to_df(search_term, src, edf)
+        logger.info(
+            f"Then finding {src} columns with edges from {search_term} in {dst} column of full edgeDataFrame"
+        )
+        df = edf[edf[src].isin(list(tdf[dst]) + [search_term])]
+    return df
+
+
+def get_graphistry_from_milieu_search(
+    search_term, src, dst, node_col, edf, ndf, both=False
+):
+    """
+        Produces large graphs of neighbors from a given search term
+    :param search_term:
+    :param src:
+    :param dst:
+    :param node_col:
+    :param edf:
+    :param ndf:
+    :param both:
+    :return:
+    """
+    tdf = get_milieu_graph_from_search(search_term, src, dst, edf, both=both)
+    gcols = pd.concat(
+        [tdf[dst], tdf[src]], axis=0
+    )  # get all node entries that show up in edge graph
+    ntdf = ndf[ndf[node_col].isin(gcols)]
+    g = graphistry.edges(tdf, src, dst).nodes(ntdf, node_col)
+    return g
